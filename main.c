e3e04964998de01168949876c0cae4407f61743e
KostyaVarenye <vekostya@gmail.com>
2022-08-03 20:08:45 +0300

    implemented ll_print, ll_insert to insert values in the beginging of the chain

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* head = NULL;
+
+void ll_insert(int x){
+    struct Node* tmp;
+    tmp = (struct Node*)malloc(sizeof(struct Node*));
+    tmp->data = x;
+    tmp->next = NULL;
+    if(head != NULL){
+      tmp->next = head;
+    }
+    head = tmp;
+
+}
+void ll_print(){
+    struct Node* tmp = head;
+    while(tmp != NULL){
+        printf("%d", tmp->data);
+        tmp = tmp->next;
+        printf(" -> ");
+    }
+    printf("\n");
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    int n, i, x;
+    printf("how many values would you like to create?\n");
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
+        printf("Enter the number \n");
+        scanf("%d", &x);
+        ll_insert(x);
+        ll_print();
+    }
     return 0;
 }
